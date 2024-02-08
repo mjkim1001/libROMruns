2fe0b7d66d58dcb293224de39360af8055422e7c
Minji Kim <minji.kim.stat@gmail.com>
2024-02-07 17:58:52 -0800

    shell file update

--- HRC/sb/sbHRC.sh
+++ HRC/sb/sbHRC.sh
@@ -3,29 +3,69 @@
 #SBATCH -t 23:00:00
 #SBATCH -p pbatch
 #SBATCH --open-mode truncate
-myPath=./dependencies/libROM/libROMruns/HRC/sb/
+myPath=./sbWin
 for t in 0.1; do
-output="${myPath}/sbEQPwin${t}.txt"
-printf "window,w,t,q,qV,qE,ErrX,ErrV,ErrE,TT1,TT2,TT3\n"  > "$output"
-for((ww=2 ;ww<=20;ww++)) do
+for rs in 2; do
+output="${myPath}/sbWin${t}.txt"
+printf "method,rs,window,t,ws,w,q,nqp,TT1,TT2,TT3,TT4,ErrX,ErrV,ErrE\n"  > "$output"
+for((ww=1 ;ww<=15;ww++)) do
 ws=$((ww*5))
-printf "${ws}"
-rm -r ./run/
-srun -n 1 -p pdebug laghos -m data/cube01_hex.mesh -pt 211 -p 1 -s 4 -tf $t -bef 1.0 -offline -romsns -rpar 0 -sample-stages -rostype interpolate -sdim 1000 -writesol > "${myPath}/sb_eqp_offline${t}ws${ws}.txt"
-srun -n 1 -p pdebug merge -nset 1 -romsns -romos -rostype interpolate -nwinsamp ${ws} -eqp -nwinover 4  > "${myPath}/sb_eqp_merge${t}ws${ws}.txt"
-w=$(find ./run -name 'basisE*' | wc -l)
-printf "${ws},${w},${t}" >> "$output"
-srun -n 1 -p pdebug laghos -m data/cube01_hex.mesh -pt 211 -p 1 -s 4 -tf $t -online -romhrprep -romsns -bef 1.0 -nwin $w -rostype interpolate -hrsamptype eqp > "${myPath}/sb_eqp_prep${t}ws${ws}.txt"
-srun -n 1 -p pdebug laghos -m data/cube01_hex.mesh -pt 211 -p 1 -s 4 -tf $t -online -romhr -romsns -bef 1.0 -nwin $w -rostype interpolate -hrsamptype eqp > "${myPath}/sb_eqp_online${t}ws${ws}.txt"
-srun -n 1 -p pdebug laghos -m data/cube01_hex.mesh -pt 211 -restore -romsns -soldiff -rostype interpolate -hrsamptype eqp -nwin $w -tf $t > "${myPath}/sb_eqp_restore${t}ws${ws}.txt"
+rm -r run/sbWin
+srun -n 1 -p pdebug laghos -o sbWin -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -offline -writesol -visit -romsvds -romos -rostype load -romsns -nwinsamp ${ws} -ef 0.9999 -sample-stages > ${myPath}/sb_offline${t}w${ww}.txt
+TT1=$(cat ${myPath}/sb_offline${t}w${ww}.txt | grep "Total time: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+w=$(find ./run/sbWin -name 'basisE*' | wc -l)
+for ((q=2; q<= 2; ++q)) do
+rm -r run/sbWinS
+rsync -a run/sbWin/. run/sbWinS/
+printf "q${q}ws${ws}sopt"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -online -romhrprep  -romos -rostype load -nwin $w -romsvds  -romgs -romsns -sfacv $q -sface $q -hrsamptype sopt > "${myPath}/sb_sopt_hyper${t}q${q}ws${ws}rs${rs}.txt"
+printf "online"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -online -romhr -romsvds -romos -rostype load -nwin $w -romgs -romsns -sfacv $q -sface $q  > "${myPath}/sb_sopt_online${t}q${q}ws${ws}rs${rs}.txt"
+printf "restore"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211  -tf $t -rs ${rs} -s 7 -restore -soldiff -romsvds  -romos -rostype load -nwin $w -romgs -romsns  > "${myPath}/sb_sopt_restore${t}q${q}ws${ws}rs${rs}.txt"
+errX=$(cat ${myPath}/sb_sopt_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Position Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+errE=$(cat ${myPath}/sb_sopt_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Energy Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+errV=$(cat ${myPath}/sb_sopt_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Velocity Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+TT2=$(cat ${myPath}/sb_sopt_hyper${t}q${q}ws${ws}rs${rs}.txt | grep "hyper-reduction preprocessing: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)' | tr '\n' ',')
+TT3=$(cat ${myPath}/sb_sopt_online${t}q${q}ws${ws}rs${rs}.txt | grep "Elapsed time for time loop: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+TT4=$(cat ${myPath}/sb_sopt_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Total time: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+nqp=$(cat ${myPath}/sb_sopt_online${t}q${q}ws${ws}rs${rs}.txt | grep "solver nqp times nzones " | egrep -wo '[0-9]*'| tr '\n' ',' )
+printf "sopt,${t},${ws},${w},${q},${nqp}${TT1},${TT2}${TT3},${TT4},${errX},${errV},${errE}\n" >> "$output"
 
-        errX=$(cat ${myPath}/sb_eqp_restore${t}ws${ws}.txt | grep "Position Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
-        errE=$(cat ${myPath}/sb_eqp_restore${t}ws${ws}.txt | grep "Energy Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
-        errV=$(cat ${myPath}/sb_eqp_restore${t}ws${ws}.txt | grep "Velocity Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
-        TT1=$(cat ${myPath}/sb_eqp_merge${t}ws${ws}.txt | grep "Elapsed time for merge: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
-        TT2=$(cat ${myPath}/sb_eqp_online${t}ws${ws}.txt | grep "Elapsed time for time loop: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
-        TT3=$(cat ${myPath}/sb_eqp_restore${t}ws${ws}.txt | grep "Total time: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+rm -r run/sbWinS
+rsync -a run/sbWin/. run/sbWinS/
+printf "q${q}ws${ws}qdeim"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -online -romhrprep  -romos -rostype load -nwin $w -romsvds  -romgs -romsns -sfacv $q -sface $q -hrsamptype qdeim > "${myPath}/sb_qdeim_hyper${t}q${q}ws${ws}rs${rs}.txt"
+printf "online"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -online -romhr -romsvds -romos -rostype load -nwin $w -romgs -romsns -sfacv $q -sface $q  > "${myPath}/sb_qdeim_online${t}q${q}ws${ws}rs${rs}.txt"
+printf "restore"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211  -tf $t -rs ${rs} -s 7 -restore -soldiff -romsvds  -romos -rostype load -nwin $w -romgs -romsns  > "${myPath}/sb_qdeim_restore${t}q${q}ws${ws}rs${rs}.txt"
+errX=$(cat ${myPath}/sb_qdeim_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Position Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+errE=$(cat ${myPath}/sb_qdeim_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Energy Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+errV=$(cat ${myPath}/sb_qdeim_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Velocity Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+TT2=$(cat ${myPath}/sb_qdeim_hyper${t}q${q}ws${ws}rs${rs}.txt | grep "hyper-reduction preprocessing: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)' | tr '\n' ',')
+TT3=$(cat ${myPath}/sb_qdeim_online${t}q${q}ws${ws}rs${rs}.txt | grep "Elapsed time for time loop: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+TT4=$(cat ${myPath}/sb_qdeim_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Total time: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+nqp=$(cat ${myPath}/sb_qdeim_online${t}q${q}ws${ws}rs${rs}.txt | grep "solver nqp times nzones " | egrep -wo '[0-9]*'| tr '\n' ',' )
+printf "qdeim,${t},${ws},${w},${q},${nqp}${TT1},${TT2}${TT3},${TT4},${errX},${errV},${errE}\n" >> "$output"
 
-        printf ",${errX},${errV},${errE},${TT1},${TT2},${TT3}\n" >> "$output"
+rm -r run/sbWinS
+rsync -a run/sbWin/. run/sbWinS/
+printf "q${q}ws${ws}gnat"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -online -romhrprep  -romos -rostype load -nwin $w -romsvds  -romgs -romsns -sfacv $q -sface $q -hrsamptype gnat > "${myPath}/sb_gnat_hyper${t}q${q}ws${ws}rs${rs}.txt"
+printf "online"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -online -romhr -romsvds -romos -rostype load -nwin $w -romgs -romsns -sfacv $q -sface $q  > "${myPath}/sb_gnat_online${t}q${q}ws${ws}rs${rs}.txt"
+printf "restore"
+srun -n 1 -p pdebug laghos -o sbWinS -p 1 -m data/cube01_hex.mesh -pt 211  -tf $t -rs ${rs} -s 7 -restore -soldiff -romsvds  -romos -rostype load -nwin $w -romgs -romsns  > "${myPath}/sb_gnat_restore${t}q${q}ws${ws}rs${rs}.txt"
+errX=$(cat ${myPath}/sb_gnat_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Position Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+errE=$(cat ${myPath}/sb_gnat_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Energy Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+errV=$(cat ${myPath}/sb_gnat_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Velocity Rel. DIFF norm " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+TT2=$(cat ${myPath}/sb_gnat_hyper${t}q${q}ws${ws}rs${rs}.txt | grep "hyper-reduction preprocessing: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)' | tr '\n' ',')
+TT3=$(cat ${myPath}/sb_gnat_online${t}q${q}ws${ws}rs${rs}.txt | grep "Elapsed time for time loop: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+TT4=$(cat ${myPath}/sb_gnat_restore${t}q${q}ws${ws}rs${rs}.txt | grep "Total time: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
+nqp=$(cat ${myPath}/sb_gnat_online${t}q${q}ws${ws}rs${rs}.txt | grep "solver nqp times nzones " | egrep -wo '[0-9]*'| tr '\n' ',' )
+printf "gnat,${t},${ws},${w},${q},${nqp}${TT1},${TT2}${TT3},${TT4},${errX},${errV},${errE}\n" >> "$output"
+done
+done
 done
 done

e638ede6e9c4cbccbdcb133cf9bd1c3c6a25e2c1
Minji Kim <minji.kim.stat@gmail.com>
2024-02-07 20:13:06 -0500

    Update sbHRCWin.sh

--- HRC/sb/sbHRCWin.sh
+++ HRC/sb/sbHRCWin.sh
@@ -8,13 +8,13 @@ for t in 0.1; do
 for rs in 2;do
 	output="${myPath}/sbWin${t}rerun.txt"
 #printf "method,t,ws,nsr,TT1,TT3,TT4,ErrX,ErrV,ErrE\n"  > "$output"
-for((ww=11 ;ww<=15;ww++)) do
+for((ww=1 ;ww<=15;ww++)) do
 ws=$((ww*5))
 rm -r run/sbWin
 srun -n 1 -p pdebug laghos -o sbWin -p 1 -m data/cube01_hex.mesh -pt 211 -tf $t -rs ${rs} -s 7 -offline -writesol -visit -romsvds -romos -rostype load -romsns -nwinsamp ${ws} -ef 0.9999 -sample-stages > ${myPath}/sb_offline${t}w${ww}.txt
 TT1=$(cat ${myPath}/sb_offline${t}w${ww}.txt | grep "Elapsed time for time loop: " | egrep -wo 'nan|[0-9]*\.([0-9]*|[0-9]*e\-[0-9]*|[0-9]*e\+[0-9]*)')
 w=$(find ./run/sbWin -name 'basisE*' | wc -l)
-for ((q=2; q<= 5; ++q)) do
+for ((q=2; q<= 2; ++q)) do
 rm -r run/sbWinS
 rsync -a run/sbWin/. run/sbWinS/
 printf "q${q}ws${ws}sopt"
